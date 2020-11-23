13f4bd31324cda58fb51fd6fbe5d2e83e47bc776
Mikola-K <hotoneshot.ua@gmail.com>
2020-11-23 13:51:21 +0200

    my first commit

---
+++ lab3c++.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>  
+
+void main() {
+
+    char br[128] ;
+    int i = 0;
+
+    scanf("%127s", br);
+
+    while (br[i] ) {
+        i++;
+    }
+    printf("size = %d", i);
+}
+
+
